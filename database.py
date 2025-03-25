4d5910e7a8c8b6b6b259ff81f040d302dffccce6
Elena Faillace <ele.faillace@gmail.com>
2025-03-25 17:30:23 +0100

    quality check added

--- database.py
+++ database.py
@@ -53,4 +53,75 @@ def add_link(url: str):
             conn.close()
     return None
 
+def quality_check():
+    """Remove rows if:
+    - URL is empty or NULL
+    - URL is duplicated
+    - URL is not a valid URL
+    """
+    try:
+        # Create connection and cursor
+        conn = sqlite3.connect('bookmarks.db')
+        cur = conn.cursor()
+        
+        # Check for empty URLs
+        res = cur.execute('''
+            SELECT * FROM links
+            WHERE URL IS NULL OR URL = ''
+        ''').fetchall()
+        if res != []:
+            cur.execute('''
+                DELETE FROM links
+                WHERE URL IS NULL OR URL = ''
+            ''')
+            print("[red]...empty URLs removed:[/]")
+            for r in res:
+                print(f'\t{r}')
+        conn.commit()
 
+        # Check for duplicated URLs
+        res = cur.execute('''
+            SELECT * FROM links
+            WHERE id NOT IN (
+                SELECT MIN(id)
+                FROM links
+                GROUP BY URL
+            )
+        ''').fetchall()
+        if res != []:
+            cur.execute('''
+                DELETE FROM links
+                WHERE id NOT IN (
+                    SELECT MIN(id)
+                    FROM links
+                    GROUP BY URL
+                )
+            ''')
+            print("[red]...duplicated URLs removed:[/]")
+            for r in res:
+                print(f'\t{r}')
+        conn.commit()
+
+        # Check for valid URLs
+        res = cur.execute('''
+            SELECT * FROM links
+            WHERE URL NOT LIKE 'http%'
+        ''').fetchall()
+        if res != []:
+            cur.execute('''
+                DELETE FROM links
+                WHERE URL NOT LIKE 'http%'
+            ''')
+            print("[red]...invalid URLs removed:[/]")
+            for r in res:
+                print(f'\t{r}')
+        conn.commit()
+
+        cur.close()
+
+    except sqlite3.Error as error:
+        print("[red bold]Error occurred - [/]", error)
+    finally:
+        if conn:
+            conn.close()
+    return None
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import typer
-from database import databset_init, add_link
+from database import databset_init, add_link, quality_check
 from rich import print
 from rich.console import Console
 from rich.table import Table
 
 console = Console()
-
-def main_tutorial(name: str, lastname: str = '', formal: bool = False):
-    """Say hi to NAME, optionally with a --lastname.
-
-    If --formal is used, say hi very formally.
-    """
-    if formal:
-        print(f"Good day Ms. {name} {lastname}.")
-    else:
-        print(f"Hello {name} {lastname}")
 
 def main():
     """Main loop for the app menu."""
@@ -39,6 +29,10 @@ def main():
         elif choice == 'a':
             url = input("Insert the URL:\n")
             add_link(url)
+    
+    # Before closing the app check that the URLs are valid, not duplicated and not empty
+    quality_check()
+    print("Goodbye!")
 
 if __name__ == "__main__":
     typer.run(main)
