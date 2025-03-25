00b634f98f12ecc807d119468882ef25790fefa5
Elena Faillace <ele.faillace@gmail.com>
2025-03-25 16:03:44 +0100

    added menu CLI and add url function

---
+++ database.py
@@ -0,0 +1,53 @@
+# Functions to handle the database
+import sqlite3  
+
+def databset_init():
+    """
+    Function to initialize the database, or just check it is initialised.
+    Database: 'bookmarks.db'. 
+    Tables: 'links'. 
+    """
+    try:
+        # Connect to DB and create a cursor
+        db_connection = sqlite3.connect('bookmarks.db')
+        cur = db_connection.cursor()
+
+        # Create a table if does not exists
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS links (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                URL TEXT
+            )
+        ''')
+        
+        # Close the cursor
+        cur.close()
+
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
+    finally:
+        if db_connection:
+            db_connection.close()
+    return None
+
+def add_link(url: str):
+    """Given an URL add the link to the database."""
+    try:
+        # Create connection and cursor
+        conn = sqlite3.connect('bookmarks.db')
+        cur = conn.cursor()
+        # Insert the URL
+        cur.execute('''
+            INSERT INTO links (URL)
+            VALUES (?)
+        ''', (url,))
+        conn.commit()
+        cur.close()
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
+    finally:
+        if conn:
+            conn.close()
+    return None
+
+
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import typer
+from database import databset_init, add_link
+from rich import print
+from rich.console import Console
+from rich.table import Table
 
+console = Console()
 
-def main(name: str, lastname: str = '', formal: bool = False):
+def main_tutorial(name: str, lastname: str = '', formal: bool = False):
     """Say hi to NAME, optionally with a --lastname.
 
     If --formal is used, say hi very formally.
@@ -10,6 +15,30 @@ def main(name: str, lastname: str = '', formal: bool = False):
         print(f"Good day Ms. {name} {lastname}.")
     else:
         print(f"Hello {name} {lastname}")
+
+def main():
+    """Main loop for the app menu."""
+    print("\nOpened the bookmarks manager app... press 'q' to quit.\n")
+
+    # Initialise the database
+    databset_init()
+
+    while True:
+        # Display the menu
+        menu = Table("option", "command", title="Bookmarks Manager", show_header=True)
+        menu.add_row("Add a bookmark: ", "a")
+        menu.add_row("Quit the application:", "q")
+        print("\n")
+        console.print(menu)
+        print("\n")
+        
+        # Ask for a command
+        choice = input("Insert a command:\n")
+        if choice == 'q':
+            break
+        elif choice == 'a':
+            url = input("Insert the URL:\n")
+            add_link(url)
 
 if __name__ == "__main__":
     typer.run(main)
